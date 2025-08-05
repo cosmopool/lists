6f712530339d3d2c708fa27957bbf028de4e5852
Kaio Delphino <dash.zet@gmail.com>
2025-08-05 00:24:43 -0300

    chore: sort directive sections alphabetically

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -3,7 +3,7 @@
 /// More dartdocs go here.
 library;
 
-export "src/entities/task.dart";
-export "src/database/utils.dart";
 export "src/database/database.dart";
+export "src/database/utils.dart";
 export "src/entities/node.dart";
+export "src/entities/task.dart";
